6a79cce394fc3729083eb4b7681a9d43673dfb16
ArikGuitarik <arik@elimelech.de>
2023-03-18 14:38:21 +0100

    Group tests into classes

--- tests/test_grid.py
+++ tests/test_grid.py
@@ -2,65 +2,59 @@ import pytest
 from grid import SquareGrid
 
 
-@pytest.fixture
-def dummy_objects():
-    return [{i} for i in range(9)]
+class TestSquareGrid:
+    @pytest.fixture
+    def dummy_objects(self):
+        return [{i} for i in range(9)]
 
+    def test_grid_creation(self, dummy_objects):
+        grid = SquareGrid(dummy_objects)
+        assert grid.shape == (3, 3)
+        assert grid.to_list() == dummy_objects
 
-def test_grid_creation(dummy_objects):
-    grid = SquareGrid(dummy_objects)
-    assert grid.shape == (3, 3)
-    assert grid.to_list() == dummy_objects
+    def test_reject_non_square_number_of_elements(self, dummy_objects):
+        with pytest.raises(ValueError):
+            SquareGrid(dummy_objects + [{10}])
 
+    def test_getter(self, dummy_objects):
+        grid = SquareGrid(dummy_objects)
+        assert grid.get(0, 0) == {0}
+        assert grid.get(2, 2) == {8}
+        assert grid.get(1, 2) == {5}
+        assert grid.get(2, 0) == {6}
+        with pytest.raises(IndexError):
+            grid.get(3, 1)
+        with pytest.raises(IndexError):
+            grid.get(0, 3)
 
-def test_reject_non_square_number_of_elements(dummy_objects):
-    with pytest.raises(ValueError):
-        SquareGrid(dummy_objects + [{10}])
+    def test_setter(self, dummy_objects):
+        grid = SquareGrid(dummy_objects)
+        element_to_add = {42}
+        grid.set(2, 1, element_to_add)
+        assert grid.get(2, 1) == element_to_add
+        with pytest.raises(IndexError):
+            grid.set(3, 0, element_to_add)
 
+    def test_swapping(self, dummy_objects):
+        grid = SquareGrid(dummy_objects)
+        grid.swap(0, 0, 0, 0)
+        assert grid.to_list() == dummy_objects
+        grid.swap(1, 2, 0, 1)
+        assert grid.get(0, 1) == {5}
+        assert grid.get(1, 2) == {1}
+        with pytest.raises(IndexError):
+            grid.swap(3, 0, 1, 3)
 
-def test_getter(dummy_objects):
-    grid = SquareGrid(dummy_objects)
-    assert grid.get(0, 0) == {0}
-    assert grid.get(2, 2) == {8}
-    assert grid.get(1, 2) == {5}
-    assert grid.get(2, 0) == {6}
-    with pytest.raises(IndexError):
-        grid.get(3, 1)
-    with pytest.raises(IndexError):
-        grid.get(0, 3)
+    def test_iteration(self, dummy_objects):
+        grid = SquareGrid(dummy_objects)
+        elements = []
+        for row, col in grid.get_all_coordinates():
+            elements.append(grid.get(row, col))
+        assert elements == dummy_objects
 
-
-def test_setter(dummy_objects):
-    grid = SquareGrid(dummy_objects)
-    element_to_add = {42}
-    grid.set(2, 1, element_to_add)
-    assert grid.get(2, 1) == element_to_add
-    with pytest.raises(IndexError):
-        grid.set(3, 0, element_to_add)
-
-
-def test_swapping(dummy_objects):
-    grid = SquareGrid(dummy_objects)
-    grid.swap(0, 0, 0, 0)
-    assert grid.to_list() == dummy_objects
-    grid.swap(1, 2, 0, 1)
-    assert grid.get(0, 1) == {5}
-    assert grid.get(1, 2) == {1}
-    with pytest.raises(IndexError):
-        grid.swap(3, 0, 1, 3)
-
-
-def test_iteration(dummy_objects):
-    grid = SquareGrid(dummy_objects)
-    elements = []
-    for row, col in grid.get_all_coordinates():
-        elements.append(grid.get(row, col))
-    assert elements == dummy_objects
-
-
-def test_truncation(dummy_objects):
-    extended_dummy_objects = dummy_objects + [{10}, {11}]
-    truncated_grid = SquareGrid.create_by_truncating_excess_elements(extended_dummy_objects)
-    assert truncated_grid.to_list() == dummy_objects
-    truncated_but_unchanged_grid = SquareGrid.create_by_truncating_excess_elements(dummy_objects)
-    assert truncated_but_unchanged_grid.to_list() == dummy_objects
+    def test_truncation(self, dummy_objects):
+        extended_dummy_objects = dummy_objects + [{10}, {11}]
+        truncated_grid = SquareGrid.create_by_truncating_excess_elements(extended_dummy_objects)
+        assert truncated_grid.to_list() == dummy_objects
+        truncated_but_unchanged_grid = SquareGrid.create_by_truncating_excess_elements(dummy_objects)
+        assert truncated_but_unchanged_grid.to_list() == dummy_objects
--- tests/test_images.py
+++ tests/test_images.py
@@ -5,98 +5,102 @@ from images import Image, ImageFromFile, Collage
 from grid import SquareGrid
 
 
-def test_image_creation():
-    example_value = 42
-    pixels = np.ones((64, 64, 3), dtype=np.uint8) * example_value
-    img = Image(pixels)
-    assert img.pixels[0, 0, 0] == example_value
-    assert img.pixels.shape == (64, 64, 3)
+class TestImage:
+    def test_image_creation(self):
+        example_value = 42
+        pixels = np.ones((64, 64, 3), dtype=np.uint8) * example_value
+        img = Image(pixels)
+        assert img.pixels[0, 0, 0] == example_value
+        assert img.pixels.shape == (64, 64, 3)
+
+    def test_image_creation_invalid_type(self):
+        with pytest.raises(TypeError):
+            Image(np.ones((64, 64, 3), dtype=np.float64))
+
+    def test_image_creation_invalid_shape(self):
+        with pytest.raises(ValueError):
+            Image(np.ones((10, 10, 1), dtype=np.uint8))
+
+    def test_image_resizing(self):
+        pixels = np.zeros((100, 100, 3), dtype=np.uint8)
+        pixels[0:50, 0:50, :], pixels[0:50, 50:100, :] = 20, 40
+        pixels[50:100, 0:50, :], pixels[50:100, 50:100, :] = 60, 80
+        img = Image(pixels)
+        img.resize((6, 6))
+        downsized_pixels = img.pixels
+        assert downsized_pixels.shape == (6, 6, 3)
+        probe_pixels = [downsized_pixels[1, 1, 0], downsized_pixels[1, 4, 0],
+                        downsized_pixels[4, 1, 0], downsized_pixels[4, 4, 0]]
+        np.testing.assert_allclose(probe_pixels, [20, 40, 60, 80], atol=1)
 
 
-def test_image_creation_invalid_type():
-    with pytest.raises(TypeError):
-        Image(np.ones((64, 64, 3), dtype=np.float64))
+class TestImageFromFile:
+    def test_creation(self):
+        img = ImageFromFile("data/dummy_cover.jpg")
+        assert img.pixels.shape == (100, 100, 3)
+        assert all(img.pixels[0, 0] == [254, 0, 0])
+        assert all(img.pixels[99, 99] == [0, 0, 254])
+
+    def test_failure_for_nonexistent_file(self):
+        with pytest.raises(FileNotFoundError):
+            ImageFromFile("data/nonexistentfile.jpg")
+
+    def test_reload_original(self):
+        img = ImageFromFile("data/dummy_cover.jpg")
+        img.resize((50, 50))
+        assert img.pixels.shape == (50, 50, 3)
+        img.reload_original()
+        assert img.pixels.shape == (100, 100, 3)
 
 
-def test_image_creation_invalid_shape():
-    with pytest.raises(ValueError):
-        Image(np.ones((10, 10, 1), dtype=np.uint8))
+class TestCollage:
+    @pytest.fixture
+    def example_img_grid(self):
+        num_images, img_side_length = 4, 64
+        imgs = [Image(np.ones((img_side_length, img_side_length, 3), dtype=np.uint8) * i * 25) for i in
+                range(num_images)]
+        return SquareGrid(imgs)
 
+    def test_collage_rendering(self, example_img_grid):
+        collage = Collage(example_img_grid, (128, 256))
+        assert collage.img_shape == (64, 128)
+        result = collage.render().pixels
+        assert result[0, 0, 0] == 0
+        assert result[28, 208, 2] == 25
+        assert result[90, 20, 2] == 50
+        assert result[70, 140, 1] == 75
+        assert result.shape == (128, 256, 3)
 
-def test_image_resizing():
-    pixels = np.zeros((100, 100, 3), dtype=np.uint8)
-    pixels[0:50, 0:50, :], pixels[0:50, 50:100, :] = 20, 40
-    pixels[50:100, 0:50, :], pixels[50:100, 50:100, :] = 60, 80
-    img = Image(pixels)
-    img.resize((6, 6))
-    downsized_pixels = img.pixels
-    assert downsized_pixels.shape == (6, 6, 3)
-    probe_pixels = [downsized_pixels[1, 1, 0], downsized_pixels[1, 4, 0],
-                    downsized_pixels[4, 1, 0], downsized_pixels[4, 4, 0]]
-    np.testing.assert_allclose(probe_pixels, [20, 40, 60, 80], atol=1)
+    def test_collage_img_shape_matching(self, example_img_grid):
+        collage = Collage(example_img_grid, (129, 257))
+        assert collage.img_shape == (64, 128)
 
+    def test_get_pixel_coordinates(self, example_img_grid):
+        collage = Collage(example_img_grid, (128, 128))
+        assert collage.get_top_left_pixel_coordinates(0, 0) == (0, 0)
+        assert collage.get_top_left_pixel_coordinates(1, 0) == (0, 64)
+        assert collage.get_top_left_pixel_coordinates(0, 1) == (64, 0)
+        assert collage.get_top_left_pixel_coordinates(1, 1) == (64, 64)
+        with pytest.raises(IndexError):
+            collage.get_top_left_pixel_coordinates(-1, 0)
+        with pytest.raises(IndexError):
+            collage.get_top_left_pixel_coordinates(2, 0)
+        with pytest.raises(IndexError):
+            collage.get_top_left_pixel_coordinates(0, -1)
+        with pytest.raises(IndexError):
+            collage.get_top_left_pixel_coordinates(0, 2)
 
-def test_image_from_file():
-    img = ImageFromFile("data/dummy_cover.jpg")
-    assert img.pixels.shape == (100, 100, 3)
-    assert all(img.pixels[0, 0] == [254, 0, 0])
-    assert all(img.pixels[99, 99] == [0, 0, 254])
-    img.resize((50, 50))
-    assert img.pixels.shape == (50, 50, 3)
-    img.reload_original()
-    assert img.pixels.shape == (100, 100, 3)
-
-
-@pytest.fixture
-def example_img_grid():
-    num_images, img_side_length = 4, 64
-    imgs = [Image(np.ones((img_side_length, img_side_length, 3), dtype=np.uint8) * i * 25) for i in range(num_images)]
-    return SquareGrid(imgs)
-
-
-def test_collage_rendering(example_img_grid):
-    collage = Collage(example_img_grid, (128, 256))
-    assert collage.img_shape == (64, 128)
-    result = collage.render().pixels
-    assert result[0, 0, 0] == 0
-    assert result[28, 208, 2] == 25
-    assert result[90, 20, 2] == 50
-    assert result[70, 140, 1] == 75
-    assert result.shape == (128, 256, 3)
-
-
-def test_collage_img_shape_matching(example_img_grid):
-    collage = Collage(example_img_grid, (129, 257))
-    assert collage.img_shape == (64, 128)
-
-
-def test_get_pixel_coordinates(example_img_grid):
-    collage = Collage(example_img_grid, (128, 128))
-    assert collage.get_top_left_pixel_coordinates(0, 0) == (0, 0)
-    assert collage.get_top_left_pixel_coordinates(1, 0) == (0, 64)
-    assert collage.get_top_left_pixel_coordinates(0, 1) == (64, 0)
-    assert collage.get_top_left_pixel_coordinates(1, 1) == (64, 64)
-    with pytest.raises(IndexError):
-        collage.get_top_left_pixel_coordinates(-1, 0)
-    with pytest.raises(IndexError):
-        collage.get_top_left_pixel_coordinates(2, 0)
-    with pytest.raises(IndexError):
-        collage.get_top_left_pixel_coordinates(0, -1)
-    with pytest.raises(IndexError):
-        collage.get_top_left_pixel_coordinates(0, 2)
-
-
-def test_get_grid_coordinates(example_img_grid):
-    collage = Collage(example_img_grid, (128, 128))
-    assert collage.get_grid_coordinates(0, 0) == (0, 0)
-    assert collage.get_grid_coordinates(28, 104) == (1, 0)
-    assert collage.get_grid_coordinates(90, 10) == (0, 1)
-    assert collage.get_grid_coordinates(70, 70) == (1, 1)
-    with pytest.raises(IndexError):
-        collage.get_grid_coordinates(-1, 0)
-    with pytest.raises(IndexError):
-        collage.get_grid_coordinates(128, 0)
-    with pytest.raises(IndexError):
-        collage.get_grid_coordinates(0, -1)
-    with pytest.raises(IndexError):
-        collage.get_grid_coordinates(0, 128)
+    def test_get_grid_coordinates(self, example_img_grid):
+        collage = Collage(example_img_grid, (128, 128))
+        assert collage.get_grid_coordinates(0, 0) == (0, 0)
+        assert collage.get_grid_coordinates(28, 104) == (1, 0)
+        assert collage.get_grid_coordinates(90, 10) == (0, 1)
+        assert collage.get_grid_coordinates(70, 70) == (1, 1)
+        with pytest.raises(IndexError):
+            collage.get_grid_coordinates(-1, 0)
+        with pytest.raises(IndexError):
+            collage.get_grid_coordinates(128, 0)
+        with pytest.raises(IndexError):
+            collage.get_grid_coordinates(0, -1)
+        with pytest.raises(IndexError):
+            collage.get_grid_coordinates(0, 128)
